a5e7ad3f1b8cdc5bfe8dd3b4301c6f32348ec289
Siddeshwar <siddeshwar.work@gmail.com>
2022-06-27 08:17:32 +0530

    added layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppNavbarComponent } from './layout/app-navbar/app-navbar.component';
+import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent,
+    AppLayoutComponent
   ],
   imports: [
     BrowserModule
