7df15e101660ec2e66f14acbfe3b4729f890bef3
Siddeshwar <siddeshwar.work@gmail.com>
2023-05-14 09:13:33 +0530

    enabled angular universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideClientHydration } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './layout/app-navbar/app-navbar.component';
@@ -42,6 +43,7 @@ import { PostListComponent } from './post/post-list/post-list.component';
     AppRoutingModule
   ],
   providers: [
+    provideClientHydration(),
     PostService
   ],
   bootstrap: [AppComponent]
